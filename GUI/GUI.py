87475ba41ee30f4653bdc1148c00acc1cb16b1a1
Bhavleen <datlab@mcmaster.ca>
2022-11-15 20:32:35 -0500

    Integrated images folder

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,12 +1,13 @@
 from tkinter import *
 
+
 # Set interface of welcome window
 root = Tk()
 root.title(string="Maction")
-# root_img = PhotoImage(file = "DCM/images/MacFireball.png")
+# root_img = PhotoImage(file = "GUI\image\maction_photo.jpeg")
 # root.iconphoto(False,root_img)
 root.geometry ("950x400")
-root.configure(bg = "black")
+root.configure(bg = "white")
 root.resizable(False,False)
 
 root.mainloop()
