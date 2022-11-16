b9a9d77c12b258cfdcf93ff068d3edad16ac2df3
Aaron Rajan <aaronmraj@gmail.com>
2022-11-15 20:06:18 -0500

    Created GUI folder and py file

---
+++ GUI/GUI.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Set interface of welcome window
+root = Tk()
+root.title(string="Maction")
+# root_img = PhotoImage(file = "DCM/images/MacFireball.png")
+# root.iconphoto(False,root_img)
+root.geometry ("950x400")
+root.configure(bg = "black")
+root.resizable(False,False)
+
+root.mainloop()
